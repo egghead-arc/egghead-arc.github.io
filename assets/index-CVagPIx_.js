3ef78c47e75756d467e7e18ca765449677b81c1a
Egghead Arc <ci@egghead-arc.github.io>
2024-11-13 09:20:20 +0000

    Update from https://github.com/egghead-arc/workspace/commit/9fa5842d7e8c220a43aea8f83dddf6a5bb602c27

---
+++ assets/index-CVagPIx_.js
@@ -0,0 +1,1 @@
+import{r as o}from"./main-CqMKN2OL.js";const r=" | Egghead Arc",s=t=>{o.useEffect(()=>{const e=document.title;return document.title=`${t}${r}`,()=>{document.title=e}})};export{s as u};
